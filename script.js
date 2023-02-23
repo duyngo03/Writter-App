9caf5d78702b5c2bc32a5ed32f0b1f317436773c
duyngo03 <21020293@vnu.edu.vn>
2023-02-23 23:58:54 +0700

    .

---
+++ script.js
@@ -0,0 +1,91 @@
+// var a = document.getElementById("area").value;
+// console.log(a)
+// localStorage.setItem("a",a)
+// var b = a.innerText;
+// var c = document.querySelectorAll("p")
+// console.log(b);
+// let b = a.map(function(item){
+//     var value = item.querySelectorAll("textarea").innerText;
+//     return value;
+    
+// });
+// localStorage.setItem("a", );
+// console.log(localStorage);
+// var area = document.getElementById('textarea');
+// if (area.addEventListener) {
+//   area.addEventListener('input', function() {
+//     // event handling code for sane browsers
+//   }, false);
+// } else if (area.attachEvent) {
+//   area.attachEvent('onpropertychange', function() {
+//     // IE-specific event handling code
+//   });
+// }
+// console.log(area.value);
+
+// console.log(a);
+// document.querySelector('textarea').addEventListener('input', function(event){
+// //   localStorage.setItem("a", event.target.value);
+// console.log("ngu");
+// });
+
+// var a = document.getElementById("textarea");
+// function press(){
+//     console.log(a.value);
+// }
+// var nicE = new nicEditors.findEditor('area');
+// question = nicE.getContent();
+// console.log(question);
+// bkLib.onDomLoaded(nicEditors.allTextAreas);
+// $(document).ready(function() {
+//   $('.button').click(function() {
+//     var data = $('#sample').find('.nicEdit-main').text();
+//     alert(data);
+//   });
+// });
+
+// var nicE = new nicEditors.findEditor("area");
+// question = nicE.getContent();
+
+// var nicInstance = nicEditors.findEditor('area');
+// var notes = nicInstance.getContent();
+// console.log(notes);
+
+// bkLib.onDomLoaded(nicEditors.allTextAreas);
+// $(document).ready(function() {
+//   $('.button').click(function() {
+//     var data = $('#sample').find('.nicEdit-main').text();
+//     console.log(data);
+//   });
+// });
+
+
+// setInterval( function() {
+//   var currentDoc = $("textarea").val();
+//   localStorage.savedDoc = currentDoc;
+//   console.log(currentDoc);
+// }, 1000);
+
+// $(document).ready( function() {
+//   var savedDoc = localStorage.savedDoc;
+//   $("textarea").val(savedDoc);
+  
+// });
+
+// var a= document.getElementById("sample");
+// var b= document.getElementById("area");
+// var test = document.querySelector(".test");
+// var output = document.querySelector(".button").addEventListener("click", function(e){
+//   console.log(b.value);
+//   localStorage.setItem("a", JSON.stringify(b.value));
+//   // var c = document.querySelector(".test");
+//   // // console.log(b.value);
+//   // var d = b.value.split("\n");
+//   // console.log(d);
+//   // for(var i =0; i < d.length; i++){
+//   //   console.log(d[i]);
+//   //   test.value += d[i] + "<br />";
+//   // }
+//   // c.innerHTML = localStorage.getItem("a");
+// });
+
