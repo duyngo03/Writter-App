631cf207ba1c64abca99257a35455611a8d860d2
duyngo03 <21020293@vnu.edu.vn>
2023-02-25 09:10:19 +0700

    .

--- script.js
+++ script.js
@@ -1,91 +1,64 @@
-// var a = document.getElementById("area").value;
-// console.log(a)
-// localStorage.setItem("a",a)
-// var b = a.innerText;
-// var c = document.querySelectorAll("p")
-// console.log(b);
-// let b = a.map(function(item){
-//     var value = item.querySelectorAll("textarea").innerText;
-//     return value;
+bkLib.onDomLoaded(function(){
+      new nicEditor().panelInstance('area');
+
+    var files = JSON.parse(localStorage.getItem("files")) || [];
     
-// });
-// localStorage.setItem("a", );
-// console.log(localStorage);
-// var area = document.getElementById('textarea');
-// if (area.addEventListener) {
-//   area.addEventListener('input', function() {
-//     // event handling code for sane browsers
-//   }, false);
-// } else if (area.attachEvent) {
-//   area.attachEvent('onpropertychange', function() {
-//     // IE-specific event handling code
-//   });
-// }
-// console.log(area.value);
+    document.querySelector(".button").addEventListener("click", function(e){
+      var d = new Date();   
+      var result = prompt("Tên file", "*");
+      if(result != null){
+            alert("Save Success");
+      }
+      var noidung = nicEditors.findEditor('area').getContent();
+      var newFile = {
+            name: result,
+            content:  noidung
+      }
+            // console.log(ngu);
+            
+            // console.log()
+      localStorage.setItem("files", JSON.stringify([...files,newFile]));
+      files = JSON.parse(localStorage.getItem("files")) || [];
+      files.forEach(element => {
+            console.log(element.name);
+      });
+      });
+      
+      // nicEditors.findEditor( "area" ).setContent(localStorage.getItem("a"));
+  });
+// var c= document.querySelector(".test");
+//       c.innerHTML = `${localStorage.getItem("a")}`
 
-// console.log(a);
-// document.querySelector('textarea').addEventListener('input', function(event){
-// //   localStorage.setItem("a", event.target.value);
-// console.log("ngu");
-// });
-
-// var a = document.getElementById("textarea");
-// function press(){
-//     console.log(a.value);
-// }
-// var nicE = new nicEditors.findEditor('area');
-// question = nicE.getContent();
-// console.log(question);
-// bkLib.onDomLoaded(nicEditors.allTextAreas);
-// $(document).ready(function() {
-//   $('.button').click(function() {
-//     var data = $('#sample').find('.nicEdit-main').text();
-//     alert(data);
-//   });
-// });
-
-// var nicE = new nicEditors.findEditor("area");
-// question = nicE.getContent();
-
-// var nicInstance = nicEditors.findEditor('area');
-// var notes = nicInstance.getContent();
-// console.log(notes);
-
-// bkLib.onDomLoaded(nicEditors.allTextAreas);
-// $(document).ready(function() {
-//   $('.button').click(function() {
-//     var data = $('#sample').find('.nicEdit-main').text();
-//     console.log(data);
-//   });
-// });
-
-
-// setInterval( function() {
-//   var currentDoc = $("textarea").val();
-//   localStorage.savedDoc = currentDoc;
-//   console.log(currentDoc);
-// }, 1000);
-
-// $(document).ready( function() {
-//   var savedDoc = localStorage.savedDoc;
-//   $("textarea").val(savedDoc);
-  
-// });
-
-// var a= document.getElementById("sample");
-// var b= document.getElementById("area");
-// var test = document.querySelector(".test");
-// var output = document.querySelector(".button").addEventListener("click", function(e){
-//   console.log(b.value);
-//   localStorage.setItem("a", JSON.stringify(b.value));
-//   // var c = document.querySelector(".test");
-//   // // console.log(b.value);
-//   // var d = b.value.split("\n");
-//   // console.log(d);
-//   // for(var i =0; i < d.length; i++){
-//   //   console.log(d[i]);
-//   //   test.value += d[i] + "<br />";
-//   // }
-//   // c.innerHTML = localStorage.getItem("a");
-// });
-
+var show = document.querySelector(".show");
+var array = JSON.parse(localStorage.getItem("files")) || [];
+var sample =document.getElementById("sample");
+var side_bar = document.querySelector(".side-bar");
+var tools = document.querySelector(".tools");
+// var textarea = document.getElementById("area");
+show.innerHTML = `<h1>Last file opened</h1>
+${array.map(file => {
+      
+      return `<button onclick="handleClick(this)" value=${file.name}>${file.name}</button>`
+    }).join(" ")}`;
+var handleClick = function(e){
+      alert(e.value); 
+      var prg =  array.find(file => file.name === e.value);
+      sample.classList.remove("hidden");
+      show.remove();
+      // side_bar.classList.add("hidden");
+      nicEditors.findEditor( "area" ).setContent(prg.content);
+      side_bar.innerHTML =`<h2>Last Open File</h2>
+                        ${array.map(file => {
+            return `<button onclick="handleClick(this)" value=${file.name}>${file.name}</button>`
+          }).join(" ")}`;
+      
+}
+tools.addEventListener("click", function(){
+      sample.classList.remove("hidden");
+      show.remove();
+      side_bar.innerHTML =`<h2>Last Open File</h2>
+                  ${array.map(file => {
+            return `<button onclick="handleClick(this)" value=${file.name}>${file.name}</button>`
+          }).join(" ")}`;
+      
+})
